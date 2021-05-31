891ac74a527cb1ba68f94e35cef579ec83e5b00f
admin <admin@YD03-00>
2021-05-31 18:26:54 +0900

    4일차 과제

---
+++ 4일차/doc/TestApp.java
@@ -0,0 +1,13 @@
+package day02;
+
+public class TestApp {
+	public static void main(String[] args) {
+		DataType dt = new DataType();
+		dt.sum(10, 20);
+		dt.suml(21000000000l, 20000000000l);
+		dt.sumd(15.5, 16,5);
+		dt.upper('a');
+		dt.sum8(077, 06);
+		dt.sum16(0xaa, 0x10);
+	}
+}
