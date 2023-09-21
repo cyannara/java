84c54fd89ac7d280dcc6ee8672da6a63dc3947ba
chichi <cyannara@naver.com>
2023-09-21 14:20:51 +0900

    array , memory 폴더 추가

---
+++ java_book/src/memory/ExceptionTest.java
@@ -0,0 +1,18 @@
+package memory;
+
+public class ExceptionTest {
+
+	public static void main(String[] args) {
+
+		int[] a = new int[5];
+		System.out.println(a[0]);
+		//System.out.println(a[5]);
+		
+		int[] b =  null;
+		if(b != null) {
+			System.out.println(b[0]);
+		}
+		
+	}
+
+}
