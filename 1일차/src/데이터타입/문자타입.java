15c16fdb093aaa1feaf456b2ac00fd089837da32
chichi <cyannara@naver.com>
2023-11-28 18:09:23 +0900

    데이터타입 예제 수정

--- 1일차/src/데이터타입/문자타입.java
+++ 1일차/src/데이터타입/문자타입.java
@@ -3,23 +3,24 @@ package 데이터타입;
 public class 문자타입 {
 	public static void main(String[] args) {
 		
-		//대소문자 바꾸기
-		char c1 = 'A';  // 
+		//문자형
+		char c1 = 'A';   
 		int c2 = 'A';
 		System.out.println(c1);
 		System.out.println((int)c1);	
 		
+		//대소문자 바꾸기
 		c2 = c1 + 32;
 		System.out.println( c2 + " " +  (char)c2  );
 		
-		//한글 표현
+		//한글 표현(유니코드)
 		c1 = '가';
 		c1 = '\uac00';
 		c1 = 0xac00;
 		c1 = 44032;
 		
-		System.out.println(c1);						//문자값
-		System.out.println((int)c1); 				//코드 10진수
+		System.out.println(c1);						 //문자값
+		System.out.println((int)c1); 				 //코드 10진수
 		System.out.println(Integer.toHexString(c1)); //16진수
 		
 		//문자열
--- 1일차/src/데이터타입/숫자타입.java
+++ 1일차/src/데이터타입/숫자타입.java
@@ -1,63 +1,77 @@
 package 데이터타입;
 /**
- * date : 2017/10/16 
+ * date : 2023/10/16 
  * @author kim yoomi
- * @since jdk1.2
  * @version 1.8
- * 변수 테스트
+ * 
+ * 숫자형 타입에 대한 연습프로그램
+ * 
  * 1. 상수값도 데이터형이 있다.
- * 정수형 상수 -int,   실수형 상수 - double
- *         10L               10.456F
+ *    정수형 상수
+ *               10    (int)   
+ *               10L   (long)
+ *         진수      
+ *               0b111 (2진수  0b 시작)
+ *               0777  (8진수  0  시작)
+ *               0xfff (16진수 0x 시작)
+ *                
+ *    실수형 상수 
+ *               10.0 (double) 
+ *               10.0F (float)  
+ *                            
  * 2. 정수형 최대값(표현범위)
- *    실수형 유효자릿수     
- * 3. 데이터형 :  boolean
- *             char
- *             byte, short, int, long
- *             float, double            
- */
-/*
- * 데이터형 연습
- *                200    200L    10.5F  10.5
- *                기본                    기본
- *   1     2       4      8       4       8
- * byte > short > int > long                          정수
- *                              > float > double      실수
- *        char                                        문자
- * boolean                                            논리   
+ *    실수형 유효자릿수 
+ *    오버플로우
+ * 
+ * 3. 숫자형 데이터타입          
+ * 
+ *                정수형상수               실수형상수
+ *                   200   200L   10.5F   10.5
+ *   1        2       4      8             
+ * byte  >  short >  int  > long     4       8        정수
+ *                               > float > double     실수
+ *                                 
  */
 public class 숫자타입 {
 	public static void main(String[] args) {
 		
-		//최대값, 오버플로우
-		int i = Integer.MAX_VALUE ;
-		System.out.println("정수형 최대값:" + i );
-		System.out.println("정수형 최대값 + 1:" + (i+2) );
+		//int형 최대값
+		int i = 200;
+		i = Integer.MAX_VALUE ;
+		System.out.println("int형 최대값: " + i );
+
+		//오버플로우
+		System.out.println("int형 최대값 + 1: " + (i+1) );
+
+		//long형 최대값
+		long l = 2200000000L;    
+		l = Long.MAX_VALUE;      //Long
+		System.out.println("long형 최대값: " + l );
 		
-		long l =Long.MAX_VALUE;   //Long
-		System.out.println("롱 정수형 최대값:" + l );
-		
-		long a = 2200000000L;    //
-		
+		//float형		
 		float f = 0.123456789f;           //유효자릿수 7자리
 		System.out.println(f);
 		
+		//double형
 		double d = 0.1234567890123456789; //유효자릿수 16자리
 		System.out.println(d);
 		
-		//진수표현   2진수:0b   8진수:0  16진수:0x
+		//진수표현 :  2진수:0b   8진수:0  16진수:0x
 		//0으로 시작하면 8진수
-		int  b = 0111;            	
-		System.out.println("8진수 111=" + b);
+		int  b = 077;            	
+		System.out.println("8진수 77 = " + b);
 		
-		int b8 = 015 + 04;
+		int b8 = 07 + 02;
 		//10진수를 8진수로 변환
-		System.out.println(b8 + " " + Integer.toOctalString(b8)); 		
+		System.out.println("07 + 02 = " + Integer.toOctalString(b8)); 		
+		
 		//0x로 시작하면 16진수
 		int b16 = 0xFF;			  	
-		System.out.println(b16);		
+		System.out.println("0xFF = " + b16);		
+		
 		//0b로 시작하면 2진수
 		int b2 = 0b1111;			
-		System.out.println(b2);
+		System.out.println("0b1111 = " + b2);
 		
 	}
 }
