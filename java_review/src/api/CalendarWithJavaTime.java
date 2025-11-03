42f9bfa2d72e9d046c8fa785d47bdac598802930
yumi <cyannara@naver.com>
2025-11-03 15:23:10 +0900

    Update CalendarWithJavaTime.java

--- java_review/src/api/CalendarWithJavaTime.java
+++ java_review/src/api/CalendarWithJavaTime.java
@@ -14,15 +14,15 @@ public class CalendarWithJavaTime {
 
 	        LocalDate firstDay = LocalDate.of(year, month, 1);
 	        int lastDay = firstDay.lengthOfMonth();
-	        DayOfWeek startDay = firstDay.getDayOfWeek(); // MONDAY, TUESDAY...
+	        DayOfWeek startDay = firstDay.getDayOfWeek(); // MONDAY=1, TUESDAY...
 
-	        System.out.println("일\t월\t화\t수\t목\t금\t토");
+        System.out.println("일\t월\t화\t수\t목\t금\t토");
 
-	        // 첫 주 공백
-	        int firstDayOfWeek = startDay.getValue(); // 일요일=0, 월요일=1 ...
-	        for (int i = 1; i < firstDayOfWeek; i++) {
-	            System.out.print("\t");
-	        }
+        // 첫 주 공백
+        int firstDayOfWeek = startDay.getValue( ); // 일요일=0, 월요일=1 ...
+        for (int i = 0; i < firstDayOfWeek%7; i++) {
+            System.out.print("\t");
+        }
 
 	        // 날짜 출력
 	        for (int day = 1; day <= lastDay; day++) {
